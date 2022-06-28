ee3d5e292a655ad5c0a840ff3bb70657458c4849
Andrew Marushko <andrewmarushko@Andrews-MacBook-Pro.local>
2022-06-28 20:09:18 +0300

    arch and sign-up user

---
+++ pkg/service/service.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"github.com/andrewmarushko/todo-app"
+	"github.com/andrewmarushko/todo-app/pkg/repository"
+)
+
+type Authorization interface {
+	CreateUser(user todo.User) (int, error)
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Service struct {
+	Authorization
+	TodoItem
+	TodoList
+}
+
+func NewService(repos *repository.Repository) *Service {
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+	}
+}
